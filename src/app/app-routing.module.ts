e4c875931d262857ca07e45218e7a07eeab5bf35
waseemniazialpha <waseem.alphasquad@gmail.com>
2023-07-14 16:52:26 +0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,24 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
 import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FaqComponent } from './faq/faq.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [{
   path: "detail/:id", component: DetailComponent
 
 },
-{ path: "", component: MainComponent }
+{ path: "", component: MainComponent },
+{ path: "about", component: AboutComponent },
+{ path: "features", component: FeaturesComponent },
+{ path: "pricing", component: PricingComponent },
+{ path: "faq", component: FaqComponent },
+{ path: "blog", component: BlogComponent },
+{ path: "contact", component: ContactComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,12 @@ import { BannerComponent } from './banner/banner.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DetailComponent } from './detail/detail.component';
 import {HttpClientModule} from '@angular/common/http';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FaqComponent } from './faq/faq.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
@@ -21,6 +27,12 @@ import {HttpClientModule} from '@angular/common/http';
     FooterComponent,
     BannerComponent,
     DetailComponent,
+    AboutComponent,
+    FeaturesComponent,
+    PricingComponent,
+    FaqComponent,
+    BlogComponent,
+    ContactComponent,
 
   ],
   imports: [
