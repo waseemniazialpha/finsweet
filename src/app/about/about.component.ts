3268346603321623074f08c6dd1850101f9f51b1
waseemniazialpha <waseem.alphasquad@gmail.com>
2023-07-18 21:58:46 +0500

    tuesdayUpdate

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  jsonData: any[] = [
+    {
+        "id":1,
+        "url":"../../assets/images/team1.png",
+        "name":"John Smith",
+        "post":"CEO"
+    },
+    {
+        "id":2,
+        "url":"../../assets/images/team2.png",
+        "name":"Simon Adams",
+        "post":"CTO"
+    },
+    {
+        "id":3,
+        "url":"../../assets/images/team3.png",
+        "name":"Paul Jones",
+        "post":"Design Lead"
+    },
+    {
+        "id":4,
+        "url":"../../assets/images/team4.png",
+        "name":"Sara Hardin",
+        "post":"Project Manager"
+    }
 
+  ];
+  gData: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.myData();
+  }
+
+    
+  myData(): void {
+    this.http.get<any>('assets/db.json').subscribe((result) => {
+      this.gData = result.gData.slice(0, 3); 
+      console.log(this.gData);
+    });
+  }
+  
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { forkJoin } from 'rxjs';
 
@@ -7,21 +7,90 @@ import { forkJoin } from 'rxjs';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
-export class MainComponent {
-  myData: any;
+export class MainComponent  implements OnInit {
+  jsonData: any[] = [
+    {
+        "id":1,
+        "num":"01",
+        "title":"How much time does it take?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":2,
+        "num":"02",
+        "title":"What is your class naming convention?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":3,
+        "num":"03",
+        "title":"How do you communicate?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":4,
+        "num":"04",
+        "title":"I have a bigger project. Can you handle it?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":5,
+        "num":"05",
+        "title":"What is your class naming convention?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ];
+  gData: any;
+  combinedData: any[] = [];
+  showParagraph: boolean = false;
 
-  constructor(private http: HttpClient) {
-  }
- 
+  constructor(private http: HttpClient) { }
+
   ngOnInit(): void {
+    this.myData();
     this.getData();
   }
-  getData(): void {
+  currentItemIndex: number = -1;
+
+  toggleParagraph(index: number) {
+    if (this.currentItemIndex === index) {
+      this.currentItemIndex = -1; // Hide the currently shown <p> tag
+    } else {
+      this.currentItemIndex = index; // Show the <p> tag for the clicked item
+    }
+  }
+  
+  
+  
+  myData(): void {
     this.http.get<any>('assets/db.json').subscribe((result) => {
-      this.myData = result.gData;
-      console.log(this.myData);
+      this.gData = result.gData;
+      console.log(this.gData);
     });
   }
+
+  getData(): void {
+    forkJoin([
+      this.http.get<any>('https://jsonplaceholder.typicode.com/photos'),
+      this.http.get<any>('https://jsonplaceholder.typicode.com/comments')
+    ]).subscribe((data: any[]) => {
+      const photosData = data[0].slice(0, 3);
+      const commentsData = data[1].slice(0, 3);
+
+      const mergedData = photosData.map((photo: any, index: number) => {
+        return {
+          ...photo,
+          body: commentsData[index]?.body,
+          name: commentsData[index]?.name
+        };
+      });
+      
+
+      this.combinedData = mergedData;
+
+      // console.log(this.combinedData);
+    });
   }
+}
 
 
