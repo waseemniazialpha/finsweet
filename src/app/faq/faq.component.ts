a4673eb1093ffab24514d3c2453f4fce81bb84ec
waseemniazialpha <waseem.alphasquad@gmail.com>
2023-07-19 16:14:51 +0500

    updatedwednesday

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent {
+  jsonData: any[] = [
+    {
+        "id":1,
+        "num":"01",
+        "title":"How much time does it take?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":2,
+        "num":"02",
+        "title":"What is your class naming convention?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":3,
+        "num":"03",
+        "title":"How do you communicate?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":4,
+        "num":"04",
+        "title":"I have a bigger project. Can you handle it?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        "id":5,
+        "num":"05",
+        "title":"What is your class naming convention?",
+        "detail":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  currentItemIndex: number = -1;
+
+  toggleParagraph(index: number) {
+    if (this.currentItemIndex === index) {
+      this.currentItemIndex = -1; // Hide the currently shown <p> tag
+    } else {
+      this.currentItemIndex = index; // Show the <p> tag for the clicked item
+    }
+  }
 }
--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./features.component.css']
 })
 export class FeaturesComponent {
+  gData: any;
 
+  constructor(private http: HttpClient) { }
+  myData(): void {
+    this.http.get<any>('assets/db.json').subscribe((result) => {
+      this.gData = result.gData;
+      console.log(this.gData);
+    });
+  }
+  ngOnInit(): void {
+    this.myData();
+  }
 }
